db23a3a48a60361c775a761d7c209edf0cc0ad20
j <137029997+Jagacool@users.noreply.github.com>
2023-08-26 15:18:08 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+import React from "react";
+import Cart from "./Cart";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Shopping Cart</h1>
+      <Cart />
+    </div>
+  );
+}
+
+export default App;
