e75cd6fa21cd7c26aa71f701a679666ffd588159
j <137029997+Jagacool@users.noreply.github.com>
2023-08-26 15:12:21 +0530

    Update AppReducer.js

--- src/AppReducer.js
+++ src/AppReducer.js
@@ -1,16 +1,36 @@
 const AppReducer = (state, action) => {
   switch (action.type) {
     case "ADD_TO_CART":
-      // Implement logic to add item to the cart
-      return state;
+      const newBook = {
+        id: action.payload.id,
+        title: action.payload.title,
+        price: action.payload.price,
+        quantity: 1,
+      };
+      return {
+        ...state,
+        cart: [...state.cart, newBook],
+      };
 
     case "INCREASE_QUANTITY":
-      // Implement logic to increase quantity of an item
-      return state;
+      return {
+        ...state,
+        cart: state.cart.map((item) =>
+          item.id === action.payload
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        ),
+      };
 
     case "DECREASE_QUANTITY":
-      // Implement logic to decrease quantity of an item
-      return state;
+      return {
+        ...state,
+        cart: state.cart.map((item) =>
+          item.id === action.payload && item.quantity > 1
+            ? { ...item, quantity: item.quantity - 1 }
+            : item
+        ),
+      };
 
     default:
       return state;
